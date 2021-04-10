8d02e7ae63cc28ebccb4777076eaa540d636483d
Bashir Akle <bashir.akle@evertecinc.com>
2021-04-10 12:41:08 -0500

    feat: Add developers index

---
+++ resources/lang/en/developers.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => 'Developers',
+    'create' => 'Create a developer',
+    'fields' => [
+        'name' => 'Name',
+        'email' => 'Email',
+        'level' => 'Level'
+    ],
+    'messages' => [
+        'empty' => 'No developers were found'
+    ]
+];
