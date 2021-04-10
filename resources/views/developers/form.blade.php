72a9fe7db5254d7c704a212f55762cc163b60dc7
Bashir Akle <bashir.akle@evertecinc.com>
2021-04-10 14:25:03 -0500

    fix: Place type and message inside b-field in developer form

--- resources/views/developers/form.blade.php
+++ resources/views/developers/form.blade.php
@@ -1,33 +1,39 @@
 @csrf
 <div class="columns">
     <div class="column">
-        <b-field label="@lang('developers.fields.name')">
+        <b-field
+            label="@lang('developers.fields.name')"
+            type="{{ $errors->has('name') ? 'is-danger' : '' }}"
+            message="{{ $errors->first('name') }}"
+        >
             <b-input
                 name="name"
                 value="{{ old('name', $developer->name) }}"
-                type="{{ $errors->has('name') ? 'is-danger' : '' }}"
-                message="{{ $errors->first('name') }}"
                 required
             ></b-input>
         </b-field>
     </div>
     <div class="column">
-        <b-field label="@lang('developers.fields.email')">
+        <b-field
+            label="@lang('developers.fields.email')"
+            type="{{ $errors->has('email') ? 'is-danger' : '' }}"
+            message="{{ $errors->first('email') }}"
+        >
             <b-input
                 name="name"
                 value="{{ old('name', $developer->email) }}"
-                type="{{ $errors->has('email') ? 'is-danger' : '' }}"
-                message="{{ $errors->first('email') }}"
                 required
             ></b-input>
         </b-field>
     </div>
     <div class="column">
-        <b-field label="@lang('developers.fields.level')">
+        <b-field
+            label="@lang('developers.fields.level')"
+            type="{{ $errors->has('level') ? 'is-danger' : '' }}"
+            message="{{ $errors->first('level') }}"
+        >
             <b-select
                 name="scrum-master"
-                type="{{ $errors->has('level') ? 'is-danger' : '' }}"
-                message="{{ $errors->first('level') }}"
                 placeholder="@lang('Select an option')"
                 value="{{ $developer->level }}"
                 expanded
