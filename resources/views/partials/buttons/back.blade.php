2095f0b65f902314f49d74448134c2fef6eacaca
John Edisson Ortiz <john.ortiz@placetopay.com>
2021-04-11 23:18:08 -0500

    refactor: Added texts to main action buttons

--- resources/views/partials/buttons/back.blade.php
+++ resources/views/partials/buttons/back.blade.php
@@ -1,3 +1,4 @@
 <a href="{{ $route }}" class="button" title="{{ $text ?? trans('common.back')}}">
     <b-icon icon="{{ $icon ?? 'arrow-left' }}"></b-icon>
+    <span>{{ $text ?? trans('common.back')}}</span>
 </a>
--- resources/views/partials/buttons/create.blade.php
+++ resources/views/partials/buttons/create.blade.php
@@ -1,3 +1,4 @@
 <a href="{{ $route }}" class="button" title="{{ $text ?? trans('common.create')}}">
     <b-icon icon="{{ $icon ?? 'plus' }}"></b-icon>
+    <span>{{ $text ?? trans('common.create')}}</span>
 </a>
